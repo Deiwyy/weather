cf82d8795cff528b2301172fd20e620400ee5893
Dawid Kaprol <69752072+Deiwyy@users.noreply.github.com>
2022-12-16 15:04:01 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
-const API_KEY = '4841e20dc74a42dc9ee91407221612'
+const API_KEY = '4841e20dc74a42dc9ee91407221612';
 
+const cityField = document.querySelector('.city');
+const iconField = document.querySelector('.icon');
+const degreesField = document.querySelector('.degrees');
+const descField = document.querySelector('.description');
 
-const button = document.querySelector('.show-weather-button');
-const countryInput = document.querySelector('.country-input');
-
-
-function update() {
-    
-}
-
-
-
+const searchCountryBtn = document.querySelector('.country-picker > .btn');
+const countrySearchBar = document.querySelector('.country-picker > input');
+let countryOld = 'Netherlands'; // Quick fix
+let country = countryOld;
 
 
 function successCallback(data) {
-    const curr = data.current;
-    const feelsLikeC = curr.feelslike_c;
-    const gustKmh = curr.gust_kph;
-    const precipMM = curr.precip_mm;
-    const tempC = curr.temp_c;
-    const city = data.location.name;
+    console.log(data)
+    if (!data.hasOwnProperty('error'))
+    {
+        const curr = data.current;
+        const desc = curr.condition.text;
+        const tempC = curr.temp_c;
+        const icon = curr.condition.icon.replace('64x64', '128x128');
+        const city = data.location.name;
+        
+        cityField.innerText = city;
+        iconField.innerHTML = `<img src="${icon}" alt="">`;
+        degreesField.innerText = `${tempC}°C`;
+        descField.innerText = desc;
+        countryOld = country;
+    } else {
+        console.log(data.error)
+        country = countryOld;
+    }
+
 }
 
 
@@ -27,16 +38,30 @@ function failureCallback(err) {
     alert(err);
 }
 
-
-
-button.addEventListener('click', () => {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${countryInput.value}`).then(
+function fetchData() {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${country}`).then(
         (response) => {
             response.json().then(
-            successCallback,
-            failureCallback
+                successCallback,
+                failureCallback
             );
         }, 
         failureCallback
     );
+}
+
+
+
+
+
+searchCountryBtn.addEventListener('click', () => {
+    country = countrySearchBar.value;
+    fetchData();
 })
+
+fetchData();
+
+setInterval(fetchData, 5000);
+
+
+
